e33a11e729f31d1ca28417ba980ee94016504869
Sofia Ochkalova <so.ochkalova@gmail.com>
2026-04-15 12:20:54 +0100

    delete tab identation from version statements to resolve YML error in nf-test snapshots

--- bin/parse_tax_lineage.py
+++ bin/parse_tax_lineage.py
@@ -54,8 +54,8 @@ def main():
         db_date = datetime.now().strftime('%Y-%m-%d')
 
     if args.version:
-        print(f"\tete3: {ete3.__version__}")
-        print(f"\tNCBI taxdump downloaded on: {db_date}")
+        print(f"ete3: {ete3.__version__}")
+        print(f"NCBI taxdump downloaded on: {db_date}")
         return
     
     get_tax_lineage(args.taxid, args.output, args.db_path, args.taxdump)
